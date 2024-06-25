85ed32f04f003f47a592975eee0182003a2cbef6
aayush <147874416+aayushx402@users.noreply.github.com>
2024-06-25 22:00:04 +0545

    update

--- function/src/main.rs
+++ function/src/main.rs
@@ -2,7 +2,12 @@ fn main() {
     println!("Hey!");
 
     first_fn();
+    
+    //passing single parameter
     second_fn(42); // Call second_fn with a parameter
+
+    //passing multiple parameter
+    third_fn(42,'S');
 }
 
 //simple function
@@ -13,4 +18,9 @@ fn first_fn() {
 //pass single parameter 
 fn second_fn(x: i32) { // Add parameter to function definition
     println!("The value of x is - {}", x);
+}
+
+// multiple params
+fn third_fn(x: i32, y: char) {
+    println!("The value of x is - {x} and the value of y is - {y}");
 }
