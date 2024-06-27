d9ec805a7b3f554058a83b4dd1eaf4e24da2cf47
aayush <147874416+aayushx402@users.noreply.github.com>
2024-06-27 23:31:39 +0545

    if, if-else

---
+++ if_else/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("if, if-else!");
+
+    first();
+}
+
+fn first() {
+    let no = 4;
+
+    if no > 5 {
+        println!("The condition is ture");
+     } else {
+        println!("The condition is false");
+    }
+}
