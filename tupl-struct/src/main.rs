dde7b9984accafa6ad85bda90161dcaff64f4c89
aayush <147874416+aayushx402@users.noreply.github.com>
2024-07-06 22:04:16 +0545

    tuple struct update

--- tupl-struct/src/main.rs
+++ tupl-struct/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
     println!("Tuple Struct!");
 
-    struct user (u8,u8,)
+    struct User (u8,u8,u8);
+
+    let mut user1 = User(5,19,69);
+
+    println!("{},{},{}",user1.0,user1.1,user1.2);
+
+    user1.1 = 23;
+
+    println!("{},{},{}",user1.0,user1.1,user1.2);
+
+
+    struct UserDetails(String,i16,bool);
+
+    let user2 = UserDetails(String::from("aayush"),21,true);
+    println!("Name is {}, Age of user {}, and Is he male ? {}",user2.0,user2.1,user2.2);
 
 
 }
